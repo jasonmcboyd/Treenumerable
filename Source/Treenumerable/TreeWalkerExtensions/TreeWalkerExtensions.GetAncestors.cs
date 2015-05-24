ece5df5795baa8051bf66b7e1b5c4424d783ade2
Jason Boyd <jason.boyd.ce@gmail.com>
2015-05-24 16:36:12 -0400

    Clean up TreeWalkerExtensions.GetAncestors
    Remove obsolete comments and fix the XML documentation.

--- Source/Treenumerable/TreeWalkerExtensions/TreeWalkerExtensions.GetAncestors.cs
+++ Source/Treenumerable/TreeWalkerExtensions/TreeWalkerExtensions.GetAncestors.cs
@@ -6,7 +6,7 @@ namespace Treenumerable
     public static partial class TreeWalkerExtensions
     {
         /// <summary>
-        /// Gets a nodes ancestors, starting with its parent node and ending with the root node.
+        /// Gets a node's ancestors, starting with its parent node and ending with the root node.
         /// </summary>
         /// <typeparam name="T">The type of elements in the tree.</typeparam>
         /// <param name="walker">
@@ -16,8 +16,8 @@ namespace Treenumerable
         /// The node whose ancestors are to be returned.
         /// </param>
         /// <returns>
-        /// An <see cref="System.Collections.Generic.IEnumerable<T>"/> that contains all the nodes
-        /// ancestors, up to and including the root.
+        /// An <see cref="System.Collections.Generic.IEnumerable<T>"/> that contains all of the 
+        /// node's ancestors, up to and including the root.
         /// </returns>
         public static IEnumerable<T> GetAncestors<T>(this ITreeWalker<T> walker, T node)
         {
@@ -32,13 +32,7 @@ namespace Treenumerable
                 throw new ArgumentNullException("node");
             }
 
-            // Get and return each parent node iteratively.
-            //ParentNode<T> parentNode = walker.GetParentNode(node);
-            //while (parentNode.HasValue)
-            //{
-            //    yield return parentNode.Value;
-            //    parentNode = walker.GetParentNode(parentNode.Value);
-            //}
+            // Return each ancestor node starting with the parent and ending with the root node.
             while (walker.TryGetParent(node, out node))
             {
                 yield return node;
