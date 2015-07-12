85ef8fb19b2d4e53b1484b511d7a7148c323127a
Jason Boyd <jason.boyd.ce@gmail.com>
2015-07-11 20:35:39 -0400

    Modify the IEnumerable<VirtualTree<T>>.GetChildAt() extension
    Modify the IEnumerable<VirtualTree<T>>.GetChildAt() extension so that it does not throw an argument out of range exception.
    The ArgumentOutOfRange exception makes sense when trying to get the child at a particular index for a single VirtualTree because there is no other way to express that we failed to get the child. But for IEnumerable<VirtualTree<T>>.GetChildAt() if we fail to get a child we can simply not add an item to the resulting IEnumerable. If we get no children can just return an empty IEnumerable.
    
    closes #5

---
+++ Source/Treenumerable.Tests/EnumerableOfVirtualTreeExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Treenumerable;
+using Treenumerable.Tests.TreeBuilder;
+
+namespace Treenumerable.Tests
+{
+    public class EnumerableOfVirtualTreeExtensionsTests
+    {
+        [Theory]
+        [InlineData(0, new int[] { 0, 0 })]
+        [InlineData(1, new int[] { 1, 1 })]
+        [InlineData(2, new int[] { 2, 2 })]
+        [InlineData(3, new int[] { })]
+        public void GetChildAt(int index, int[] expected)
+        {
+            IEnumerable<VirtualTree<Node<int>>> nums =
+                Enumerable
+                .Range(0, 2)
+                .Select(x =>
+                    VirtualTree
+                    .Create(
+                        new NodeWalker<int>(),
+                        Node
+                        .Create(x)
+                        .AddChildren(
+                            Enumerable
+                            .Range(0, 3)
+                            .Select(y => Node.Create(y))
+                            .ToArray())));
+
+            Assert.Equal(expected, nums.GetChildAt(index).Unwrap().Select(x => x.Value).ToArray());
+        }
+
+        [Fact]
+        public void GetChildAt_NullSource_ArgumentNullExceptionThrown()
+        {
+            Assert.Throws<ArgumentNullException>(
+                "virtualTrees",
+                () => 
+                    EnumerableOfVirtualTreeExtensions
+                    .GetChildAt<int>(null, 0)
+                    .ToArray()); 
+        }
+
+        [Fact]
+        public void GetChildAt_IndexNegative_ArgumentOutOfRangeExceptionThrown()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                "index",
+                () => 
+                    EnumerableOfVirtualTreeExtensions
+                    .GetChildAt<int>(Enumerable.Empty<VirtualTree<int>>(), -1)
+                    .ToArray());
+        }
+    }
+}
--- Source/Treenumerable/VirtualTreeExtensions/EnumerableOfVirtualTreeExtensions.cs
+++ Source/Treenumerable/VirtualTreeExtensions/EnumerableOfVirtualTreeExtensions.cs
@@ -6,6 +6,63 @@ namespace Treenumerable
 {
     public static class EnumerableOfVirtualTreeExtensions
     {
+        /// <summary>
+        /// Takes an <see cref="IEnumerable&lt;VirtualTree&lt;T&gt;&gt;"/> and returns each 
+        /// <see cref="VirtualTree&lt;T&gt;"/>'s child at the specified index.  If the index is
+        /// greater than the number of children then no child is returned for that 
+        /// <see cref="VirtualTree&lt;T&gt;"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the tree.</typeparam>
+        /// <param name="virtualTrees">
+        /// The <see cref="VirtualTree&lt;T&gt;"/>'s that are to have their child returned.
+        /// </param>
+        /// <param name="index">
+        /// The index of the child to be returned.
+        /// </param>
+        /// <returns>
+        /// An <see cref="IEnumerable&lt;VirtualTree&lt;T&gt;&gt;"/> representing the selected
+        /// child from each <see cref="VirtualTree&lt;T&gt;"/>.
+        /// </returns>
+        public static IEnumerable<VirtualTree<T>> GetChildAt<T>(
+            this IEnumerable<VirtualTree<T>> virtualTrees,
+            int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("index");
+            }
+
+            if (virtualTrees == null)
+            {
+                throw new ArgumentNullException("virtualTrees");
+            }
+
+            foreach (VirtualTree<T> tree in virtualTrees)
+            {
+                IList<T> list = tree.TreeWalker.GetChildren(tree.Root) as IList<T>;
+                if (list != null)
+                {
+                    if (index < list.Count)
+                    {
+                        yield return tree.CreateFromSelf(list[index]);
+                    }
+                }
+                else
+                {
+                    int count = 0;
+                    foreach (T node in tree.TreeWalker.GetChildren(tree.Root))
+                    {
+                        if (count == index)
+                        {
+                            yield return tree.CreateFromSelf(node);
+                            break;
+                        }
+                        count++;
+                    }
+                }
+            }
+        }
+
         public static IEnumerable<VirtualTree<T>> SelectDescendants<T>(
             this IEnumerable<VirtualTree<T>> virtualTrees, 
             Func<T, bool> predicate)
@@ -85,15 +142,6 @@ namespace Treenumerable
             return
                 virtualTrees
                 .SelectMany(x => x.SelectChildren(key, comparer));
-        }
-
-        public static IEnumerable<VirtualTree<T>> GetChildAt<T>(
-            this IEnumerable<VirtualTree<T>> virtualTrees,
-            int index)
-        {
-            return
-                virtualTrees
-                .Select(x => x.GetChildAt(index));
         }
 
         public static IEnumerable<VirtualTree<T>> GetChildAtOrDefault<T>(
