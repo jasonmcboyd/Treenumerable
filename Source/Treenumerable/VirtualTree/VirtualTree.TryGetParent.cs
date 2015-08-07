984fb742940a6370d7b60953cdd8dc235232c9a8
Jason Boyd <jason.boyd.ce@gmail.com>
2015-08-06 21:29:58 -0400

    Add missing VirtualTree.TryGetParent method

---
+++ Source/Treenumerable/VirtualTree/VirtualTree.TryGetParent.cs
@@ -0,0 +1,20 @@
+﻿namespace Treenumerable
+{
+    public partial struct VirtualTree<T>
+    {
+        public bool TryGetParent(out VirtualTree<T> parent)
+        {
+            T parentNode;
+            if (this.TreeWalker.TryGetParent(this.Root, out parentNode))
+            {
+                parent = this.ShallowCopy(parentNode);
+                return true;
+            }
+            else
+            {
+                parent = default(VirtualTree<T>);
+                return false;
+            }
+        }
+    }
+}
