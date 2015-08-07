d4eb0d7b141349ab5dff81a832b6c2dfd5bb34a7
Jason Boyd <jason.boyd.ce@gmail.com>
2015-08-06 22:04:56 -0400

    Add missing VirtualTree.GetParent method

---
+++ Source/Treenumerable/VirtualTree/VirtualTree.GetParent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Treenumerable
+{
+    public partial struct VirtualTree<T>
+    {
+        public VirtualTree<T> GetParent()
+        {
+            // Return the node's parent or a default node if the parent does not exist.
+            VirtualTree<T> parent;
+            if (this.TryGetParent(out parent))
+            {
+                return parent;
+            }
+            else
+            {
+                throw new InvalidOperationException("The node does not have a parent.");
+            }
+        }
+    }
+}
