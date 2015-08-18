0ccaf63b1cc60a30271af8636b82534370b4a9ca
Jason Boyd <jason.boyd.ce@gmail.com>
2015-08-17 21:45:16 -0400

    Fix GetDescendantsOfType
    Because why not.  Still needs to be finalized.

--- Source/Treenumerable/TreeWalkerExtensions/TreeWalkerExtensions.GetDescendantsOfType.cs
+++ Source/Treenumerable/TreeWalkerExtensions/TreeWalkerExtensions.GetDescendantsOfType.cs
@@ -1,52 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Treenumerable
 {
     public static partial class TreeWalkerExtensions
     {
-        public static IEnumerable<TResult> GetDescendantsOfType<TSource, TResult>(this ITreeWalker<TSource> walker, IEnumerable<TSource> nodes, bool includeNodes)
+        
+        public static IEnumerable<TResult> GetDescendantsOfType<TSource, TResult>(
+            this ITreeWalker<TSource> walker,
+            IEnumerable<TSource> nodes)
         {
+            // Validate parameters.
+            if (walker == null)
+            {
+                throw new ArgumentNullException("walker");
+            }
+
             if (nodes == null)
             {
-                yield break;
+                throw new ArgumentNullException("nodes");
             }
 
-            foreach (object node in nodes)
+            foreach (object descendant in walker.GetDescendants(nodes, n => n is TResult))
             {
-                if (includeNodes && node is TResult)
-                {
-                    yield return (TResult)node;
-                }
-                else
-                {
-                    foreach (TResult descendant in walker.GetDescendantsOfType<TSource, TResult>(walker.GetChildren((TSource)node), true))
-                    {
-                        yield return descendant;
-                    }
-                }
+                yield return (TResult)descendant;
             }
         }
-
-        //public static IEnumerable<T> GetDescendants<T>(this ITreeWalker<T> walker, IEnumerable<T> nodes, Func<T, bool> predicate)
-        //{
-        //    return walker.GetDescendants(nodes, predicate, false);
-        //}
-
-        //public static IEnumerable<T> GetDescendants<T>(this ITreeWalker<T> walker, T node, Func<T, bool> predicate, bool includeNode)
-        //{
-        //    if (node == null)
-        //    {
-        //        return Enumerable.Empty<T>();
-        //    }
-
-        //    return walker.GetDescendants(Enumerable.Repeat(node, 1), predicate, includeNode);
-        //}
-
-        //public static IEnumerable<T> GetDescendants<T>(this ITreeWalker<T> walker, T node, Func<T, bool> predicate)
-        //{
-        //    return walker.GetDescendants<T>(node, predicate, false);
-        //}
     }
 }
