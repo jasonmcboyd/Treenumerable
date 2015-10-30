2d47fda0e507481f693b2439de378ea8a820ece2
Jason Boyd <jason.boyd.ce@gmail.com>
2015-10-30 12:45:46 -0400

    Split TreeWalkerExtensions.GetChildren into two files
    Moved the four method overloads that search for children by key to their own file: TreeWalkerExtensions.GetChildrenByKey.cs.

---
+++ Source/Treenumerable/TreeWalkerExtensions/TreeWalkerExtensions.GetChildrenByKey.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Treenumerable
+{
+    public static partial class TreeWalkerExtensions
+    {
+        /// <summary>
+        /// Gets the children that match the <paramref name="key"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the tree.</typeparam>
+        /// <param name="walker">
+        /// The <see cref="ITreeWalker&lt;T&gt;"/> that knows how to find the parent and child
+        /// nodes.
+        /// </param>
+        /// <param name="nodes">
+        /// The root nodes to be queried.
+        /// </param>
+        /// <param name="key">
+        /// The key that each child will be compared to.
+        /// </param>
+        /// <returns>
+        /// An <see cref="System.Collections.Generic.IEnumerable&lt;T&gt;"/> that contains all the
+        /// matching children.
+        /// </returns>
+        public static IEnumerable<T> GetChildren<T>(
+            this ITreeWalker<T> walker,
+            IEnumerable<T> nodes,
+            T key)
+        {
+            // Validate parameters.
+            if (walker == null)
+            {
+                throw new ArgumentNullException("walker");
+            }
+
+            if (nodes == null)
+            {
+                throw new ArgumentNullException("nodes");
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            return walker.GetChildren(nodes, key, null);
+        }
+
+        /// <summary>
+        /// Gets the children that match the <paramref name="key"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the tree.</typeparam>
+        /// <param name="walker">
+        /// The <see cref="ITreeWalker&lt;T&gt;"/> that knows how to find the parent and child
+        /// nodes.
+        /// </param>
+        /// <param name="nodes">
+        /// The root nodes to be queried.
+        /// </param>
+        /// <param name="key">
+        /// The key that each child will be compared to.
+        /// </param>
+        /// <param name="comparer">
+        /// The <see cref="IEqualityComparer&lt;T&gt;"/> used to compare the key and the child.  If
+        /// this is null then the default <see cref="EqualityComparer&lt;T&gt;.Default"/> will be
+        /// used.
+        /// </param>
+        /// <returns>
+        /// An <see cref="System.Collections.Generic.IEnumerable&lt;T&gt;"/> that contains all the
+        /// matching children.
+        /// </returns>
+        public static IEnumerable<T> GetChildren<T>(
+            this ITreeWalker<T> walker,
+            IEnumerable<T> nodes,
+            T key,
+            IEqualityComparer<T> comparer)
+        {
+            // Validate parameters.
+            if (walker == null)
+            {
+                throw new ArgumentNullException("walker");
+            }
+
+            if (nodes == null)
+            {
+                throw new ArgumentNullException("nodes");
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            // If the comparer is null then use the defautl comparer for that type.
+            comparer = comparer ?? EqualityComparer<T>.Default;
+
+            return
+                nodes
+                .SelectMany(n => walker.GetChildren(n))
+                .Where(n => comparer.Equals(key, n));
+        }
+
+        /// <summary>
+        /// Gets the children that match the <paramref name="key"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the tree.</typeparam>
+        /// <param name="walker">
+        /// The <see cref="ITreeWalker&lt;T&gt;"/> that knows how to find the parent and child
+        /// nodes.
+        /// </param>
+        /// <param name="node">
+        /// The root node to be queried.
+        /// </param>
+        /// <param name="key">
+        /// The key that each child will be compared to.
+        /// </param>
+        /// <returns>
+        /// An <see cref="System.Collections.Generic.IEnumerable&lt;T&gt;"/> that contains all the
+        /// matching children.
+        /// </returns>
+        public static IEnumerable<T> GetChildren<T>(
+            this ITreeWalker<T> walker,
+            T node,
+            T key)
+        {
+            // Validate parameters.
+            if (walker == null)
+            {
+                throw new ArgumentNullException("walker");
+            }
+
+            if (node == null)
+            {
+                throw new ArgumentNullException("node");
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            return walker.GetChildren(node, key, null);
+        }
+
+        /// <summary>
+        /// Gets the children that match the <paramref name="key"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the tree.</typeparam>
+        /// <param name="walker">
+        /// The <see cref="ITreeWalker&lt;T&gt;"/> that knows how to find the parent and child
+        /// nodes.
+        /// </param>
+        /// <param name="node">
+        /// The root node to be queried.
+        /// </param>
+        /// <param name="key">
+        /// The key that each child will be compared to.
+        /// </param>
+        /// <param name="comparer">
+        /// The <see cref="IEqualityComparer&lt;T&gt;"/> used to compare the key and the child.  If
+        /// this is null then the default <see cref="EqualityComparer&lt;T&gt;.Default"/> will be
+        /// used.
+        /// </param>
+        /// <returns>
+        /// An <see cref="System.Collections.Generic.IEnumerable&lt;T&gt;"/> that contains all the
+        /// matching children.
+        /// </returns>
+        public static IEnumerable<T> GetChildren<T>(
+            this ITreeWalker<T> walker,
+            T node,
+            T key,
+            IEqualityComparer<T> comparer)
+        {
+            // Validate parameters.
+            if (walker == null)
+            {
+                throw new ArgumentNullException("walker");
+            }
+
+            if (node == null)
+            {
+                throw new ArgumentNullException("node");
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            // If the comparer is null then use the defautl comparer for that type.
+            comparer = comparer ?? EqualityComparer<T>.Default;
+
+            return
+                walker
+                .GetChildren(node)
+                .Where(n => comparer.Equals(key, n));
+        }
+    }
+}
